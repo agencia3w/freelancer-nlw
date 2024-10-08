f0af1962b4f62e23f2cd480c82fe858a4d6ef01f
Paulinho <paulinho@agencia3w.com.br>
2024-10-08 10:14:29 -0300

    Project model and seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,5 +11,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(200)->create();
+
+        User::query()->inRandomOrder()->limit(10)->get()
+            ->each(fn(User $user) => Project::factory()->create(['created_by' => $user->id]));
     }
 }
