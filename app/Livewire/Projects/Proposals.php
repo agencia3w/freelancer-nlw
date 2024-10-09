3469646fc11e76eba415df8eaa702f84b0e5815e
Paulinho <paulinho@agencia3w.com.br>
2024-10-09 17:03:54 -0300

    Timer and paginate proposals

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -3,12 +3,28 @@
 namespace App\Livewire\Projects;
 
 use App\Models\Project;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Proposals extends Component
 {
+    const LOAD_MORE_RECORDS = 10;
     public Project $project;
-    
+    public int $limit = 10;
+
+    #[Computed()]
+    public function proposals()
+    {
+        return $this->project->proposals()
+            ->orderByDesc('hours')
+            ->simplePaginate($this->limit);
+    }
+
+    public function loadMore()
+    {
+        $this->limit += self::LOAD_MORE_RECORDS;
+    }
+
     public function render()
     {
         return view('livewire.projects.proposals');
